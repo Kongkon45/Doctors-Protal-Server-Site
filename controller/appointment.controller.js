ba54116cf3d1a93d6e2802da3bcaf01fc661fe44
Kongkon Jowarder <kongkon4545@gmail.com>
2024-03-10 14:19:46 +0600

    status code change

--- controller/appointment.controller.js
+++ controller/appointment.controller.js
@@ -37,7 +37,7 @@ const createAppointment = async (req, res) => {
     });
     const appointmentData = await newAppointment.save();
 
-    res.status(201).json({
+    res.status(200).json({
       message: "Appointment created successfully",
       success: true,
       data: appointmentData,
